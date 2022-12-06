0d555bbfefe8af4a69dd89fb12094d2b3b253048
revike <revike@ya.ru>
2022-12-06 23:43:56 +0300

    front

--- testing_service/auth_app/forms.py
+++ testing_service/auth_app/forms.py
@@ -14,7 +14,17 @@ class UserRegisterForm(UserCreationForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
             field.help_text = ''
+            field.label = ''
+        self.fields['username'].widget.attrs[
+            'placeholder'] = 'Имя пользователя'
+        self.fields['first_name'].widget.attrs['placeholder'] = 'Имя'
+        self.fields['last_name'].widget.attrs['placeholder'] = 'Фамилия'
+        self.fields['email'].widget.attrs['placeholder'] = 'Email'
+        self.fields['password1'].widget.attrs['placeholder'] = 'Пароль'
+        self.fields['password2'].widget.attrs[
+            'placeholder'] = 'Повторите пароль'
 
 
 class UserLoginForm(AuthenticationForm):
@@ -23,3 +33,13 @@ class UserLoginForm(AuthenticationForm):
     class Meta:
         model = User
         fields = ('username', 'password')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
+            field.help_text = ''
+            field.label = ''
+        self.fields['username'].widget.attrs[
+            'placeholder'] = 'Имя пользователя'
+        self.fields['password'].widget.attrs['placeholder'] = 'Пароль'
