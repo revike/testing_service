5a16a42731f1af6e5b141a28c58d07dcbc375b05
revike <revike@ya.ru>
2022-12-07 11:07:47 +0300

    tests auth_app

--- testing_service/auth_app/tests.py
+++ testing_service/auth_app/tests.py
@@ -1,3 +1,52 @@
-from django.test import TestCase
+from django.core.management import call_command
+from django.test import TestCase, Client
+from django.urls import reverse
 
-# Create your tests here.
+
+class TestAuthApp(TestCase):
+    """Test for app auth"""
+
+    def setUp(self) -> None:
+        call_command('flush', '--noinput')
+        call_command('loaddata', 'test_db.json')
+        self.client = Client()
+        self.user = {'username': 'admin', 'password': 'admin'}
+        self.register = {'username': 'admin', 'password1': 'admin',
+                         'password2': 'admin'}
+
+    def test_register(self):
+        """Test register"""
+        url = reverse('auth:register')
+        response = self.client.get(url)
+        self.assertTrue(response.context['user'].is_anonymous)
+        self.assertEqual(response.status_code, 200)
+        response = self.client.get(reverse('main:list_test'))
+        self.assertEqual(response.status_code, 302)
+        self.client.post(url, data=self.register)
+        self.client.post(reverse('auth:login'), data=self.user)
+        response = self.client.get(reverse('main:list_test'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_login(self):
+        """Test login"""
+        url = reverse('auth:login')
+        response = self.client.get(url)
+        self.assertTrue(response.context['user'].is_anonymous)
+        self.assertEqual(response.status_code, 200)
+        response = self.client.get(reverse('main:list_test'))
+        self.assertEqual(response.status_code, 302)
+        self.client.post(url, data=self.user)
+        response = self.client.get(reverse('main:list_test'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_logout(self):
+        url = reverse('auth:logout')
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 302)
+        self.client.post(reverse('auth:login'), data=self.user)
+        response = self.client.get(reverse('main:list_test'))
+        self.assertEqual(response.status_code, 200)
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 302)
+        response = self.client.get(reverse('main:list_test'))
+        self.assertEqual(response.status_code, 302)
